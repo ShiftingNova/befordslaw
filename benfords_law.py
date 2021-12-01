6827ce45b4ddc14f5de1c79e91fe98d2e0304b87
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-12-01 16:31:06 -0700

    Update benfords_law.py

--- benfords_law.py
+++ benfords_law.py
@@ -1,7 +1,9 @@
-file_name = str(input("Enter input file:\n"))
+file_name = str(input("Data file name:\n\n"))
 file = open(file_name,'r')
 numbers = []
 amount = {}
+law = True
+check = {1:30, 2:17,3:12,4:9,5:7,6:6,7:5,8:5,9:4}
 for i in file:
     o = i.strip("\n")
     e = o.split(",")
@@ -16,3 +18,10 @@ for i in numbers:
 for key in amount:
     count = int((amount[key]/len(numbers))*100)
     print(key,"|","#"*count)
+for key in amount:
+    if amount[key] > check[key]+10 or amount[key] < check[key]-5:
+        law = False
+if law:
+    print("\nFollows Benford's Law")
+else:
+    print("\nDoes not follow Benford's Law")
